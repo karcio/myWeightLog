3f04181b895e7df27f19df96530a16f3dce0f039
karcio <kczarnecki@hotmail.com>
2017-07-03 20:07:55 +0100

    finish

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # My Weight Log v 0.1
-============================
+
+- It is very easy tool to log your everyday weight. 
+- Weight is save in csv file. 
+
+Run:
+
+```sh
+$ ./myWeight.py
+```
--- src/default/myWeight.py
+++ src/default/myWeight.py
@@ -1,6 +1,25 @@
+#!/usr/bin/python
 '''
 Created on 3 Jul 2017
-test
+
 @author: crystal
 '''
 
+import csv
+import datetime
+
+
+def title():
+    print('Weight v 0.1')
+    print('====================')
+
+def insertData():
+    weight = input('Insert weight: ')
+    
+    print('done ...')
+    with open('weight.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        spamwriter.writerow(['{:%Y-%m-%d}'.format(datetime.datetime.now()), weight])
+    
+title()
+insertData()
